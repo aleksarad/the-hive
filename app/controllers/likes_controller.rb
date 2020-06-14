708c73773049ea11632e828cbfeea3822639ab69
hkofkin <hkofkin@gmail.com>
2020-06-14 15:34:07 -0400

    Like functionality added with new form

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+    def new
+        @like = Like.new
+        @posts = Post.all
+        @users = User.all
+    end
+
+    def create
+        @like = Like.create(like_params)
+        # @post = Post.find_by(id: @like.post_id)
+        # @post.add_like
+        redirect_to posts_path
+    end
+
+
+    private
+
+    def like_params
+        params.require(:like).permit(:user_id, :post_id)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,16 @@ class Post < ApplicationRecord
     def self.feed_order
         self.order(created_at: :desc)
     end
+
+
+    # adding access to likes for each post instance
+    # @@total_likes = 0
+
+    # def self.add_like
+    #     self.total_likes += 1
+    # end
+
+    # def self.remove_like
+    #     self.total_likes -= 1
+    # end
 end
