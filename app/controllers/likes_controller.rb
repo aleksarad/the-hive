a099dd4ffd7fc4730c175210d8dd8413aa94edf4
aleksarad <alexa.radevich1@gmail.com>
2020-06-14 22:30:46 -0400

    adding likes and a bunch of styling test

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,35 @@
 class LikesController < ApplicationController
+    # def new
+    #     @like = Like.new
+    # end 
+
+    # def create
+    #     @like = Like.create(like_params)
+    #     redirect_to posts_path
+    # end
+
+    def index
+        redirect_to posts_path
+    end
+
+    def add_like
+        @like = Like.create(user_id: params[:user_id], post_id: params[:post_id])
+        redirect_to posts_path
+    end
+
+    def delete
+        if already_liked?
+            @like.destroy
+        end
+        redirect_to posts_path
+    end
+
+    private
+    def like_params
+        params.require(:like).permit(:user_id, :post_id)
+    end
+
+    def already_liked?
+        Like.where(user_id: params[:user_id], post_id: params[:post_id]).exists?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :likes
+  get '/likes' => 'likes#index'
+  post '/likes' => 'likes#add_like'
+  delete '/likes' => 'likes#delete'
   resources :posts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
