17784150c662f28b65e46f1a42d7f31c8cbbdbc1
aleksarad <alexa.radevich1@gmail.com>
2020-06-16 14:38:06 -0400

    prelim styling

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,32 +1,18 @@
 class LikesController < ApplicationController
 
-    def index
-        redirect_to posts_path
-    end
-
-    def add_like
-        @like = Like.create(user_id: params[:user_id], post_id: params[:post_id])
-        redirect_to posts_path
-    end
-
-    def remove_like
-        @user = User.find_by(params[:user_id])
-        @like = @user.likes.find_by(post_id: params[:post_id])
+    def toggle_like
         @post = Post.find(params[:post_id])
-        @like.destroy!
-        redirect_to posts_path
-    end
-
-    def show
-        @user = User.find(params[:user_id])
-        @likes = Like.find_by(user_id: @user.id)
+        if current_user.liked?(@post)
+            @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+            @like.destroy
+            redirect_back(fallback_location: posts_path)
+        else
+            @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+            redirect_back(fallback_location: posts_path)
+        end
     end
 
     private
-
-    # def already_liked?
-    #     Like.where(user_id: params[:user_id], post_id: params[:post_id]).exists?
-    # end
 
     def like_params
         params.require(:like).permit(:user_id, :post_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
 
+  resources :users do
+    get 'likes', to: 'users#likes'
+  end
+  resources :posts
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/login'
   get 'sessions/welcome'
-    get '/likes' => 'likes#index'
-    post '/likes' => 'likes#add_like'
-    delete '/likes' => 'likes#remove_like'
-    resources :posts
-    resources :users do
-      resources :likes, only: [:index]
-    end
-    get 'login', to: 'sessions#new'
-    post 'login', to: 'sessions#create'
-    get 'welcome', to: 'sessions#welcome'
-    get 'authorized', to: 'sessions#page_requires_login'
-    post '/logout' => 'sessions#destroy'
+  get '/likes' => 'likes#index'
+  post '/likes' => 'likes#toggle_like'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+  post '/logout' => 'sessions#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
